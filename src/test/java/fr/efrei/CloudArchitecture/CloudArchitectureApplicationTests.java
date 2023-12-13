bb52213cfc687fccd0953ba630a6c27f623a648a
Vamlack <thomas.miras.garcia@efrei.net>
2023-12-13 14:26:17 +0100

    first project upload (dl from spring intializr)

---
+++ src/test/java/fr/efrei/CloudArchitecture/CloudArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.CloudArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
