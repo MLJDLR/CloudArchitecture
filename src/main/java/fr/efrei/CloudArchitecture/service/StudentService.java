c2bf8c6addf2dc7d7143304fb595d1076bd30b06
Vamlack <thomas.miras.garcia@efrei.net>
2023-12-13 19:07:48 +0100

    CRUD startup

--- src/main/java/fr/efrei/CloudArchitecture/service/StudentService.java
+++ src/main/java/fr/efrei/CloudArchitecture/service/StudentService.java
@@ -5,6 +5,7 @@ import fr.efrei.CloudArchitecture.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -20,6 +21,23 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public Student save(Student student){
+        return studentRepository.save(student);
+    }//Create
+
+    public Student update(Student student){
+        return studentRepository.save(student);
+    }
+
+    public Integer findStudentIdByName(String name) {
+        Optional<Student> studentOptional = studentRepository.findByName(name);
+        return studentOptional.map(Student::getId).orElse(null);
+    }
+
+    public void delete(String name){
+        Integer id = findStudentIdByName(name);
+        studentRepository.deleteById(id);
+    }
 }
 
 
--- src/main/java/fr/efrei/CloudArchitecture/web/rest/StudentResource.java
+++ src/main/java/fr/efrei/CloudArchitecture/web/rest/StudentResource.java
@@ -2,6 +2,8 @@ package fr.efrei.CloudArchitecture.web.rest;
 
 import fr.efrei.CloudArchitecture.domain.Student;
 import fr.efrei.CloudArchitecture.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/student")
 public class StudentResource {
 
     public final StudentService studentService;
@@ -20,8 +22,12 @@ public class StudentResource {
 
     @GetMapping("/items")
     public List<Student> getAllStudents(){
-
         return studentService.findAll();
     }
 
+    @DeleteMapping("/items")
+    public List<Student> deleteStudent() {
+        studentService.delete("Thomas");
+        return studentService.findAll();
+    }
 }
